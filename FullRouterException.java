7d228f4bd1450072efbb761421249281031ab951
tahseen-algo75 <110347211+tahseen-algo75@users.noreply.github.com>
2022-12-04 01:02:27 -0500

    Add files via upload

---
+++ FullRouterException.java
@@ -0,0 +1,17 @@
+/*
+ *  @author Tahseen Zaman
+ *  ID : 114332480
+ *  Recitation : 03
+ */
+
+/*
+ * This is a customized exception class...
+ */
+ public class FullRouterException extends Exception{
+    
+    public FullRouterException(){}
+    
+    public FullRouterException(String string){
+        super(string);
+    }
+}
